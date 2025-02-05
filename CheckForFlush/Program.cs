d01b28c4047117b58ef4deb7f4a32c15bb14eca0
ImPhame <jakobsenkenneth@hotmail.no>
2025-02-05 11:44:40 +0100

    x

---
+++ CheckForFlush/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using CardLib;
+
+Deck myDeck = new Deck();
+myDeck.Shuffle();
+Card[] myHand = new Card[5];
+
+
+DrawFiveCards(myDeck);
+
+
+ void DrawFiveCards(Deck deck)
+ {
+
+    for (int i = 0; i < 5; i++)
+    {
+        Card myCard = deck.GetCard(i);
+        myHand[i] = myCard;
+        Console.WriteLine(myHand[i]);
+    }
+}
+
